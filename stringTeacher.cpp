d73548826ee7fee478f6bd7ce266cebe45585ea4
A. Belard <angebelard@Gmail.com>
2022-02-02 11:19:06 +0200

    Create stringTeacher.cpp

---
+++ stringTeacher.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string firstTxt="";
+    string secondTxt="";
+
+    cout<<"Enter the first text: " <<endl;
+    cin>>firstTxt;
+
+    cout<<"Enter the first text: " <<endl;
+    cin>>secondTxt;
+
+    cout << "Combined string is: " << firstTxt +secondTxt<<endl;
+}
